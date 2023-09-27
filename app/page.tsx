249105a79509af5b008dd69daaca408713488e31
Jam516 <kufuorkofi@gmail.com>
2023-09-27 14:00:24 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function IndexPage() {
       <DesktopBlocker />
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          PWA Wallet Test
+          PWA Wallet
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
           Crypto Mobile App using Next.js
